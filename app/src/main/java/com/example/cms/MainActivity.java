f912086222a65bf580ef7637e4e2f832829967ae
srijareddy11z <srijareddysr11.com>
2023-02-06 16:05:10 +0530

    next

--- app/src/main/java/com/example/cms/MainActivity.java
+++ app/src/main/java/com/example/cms/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.cms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn2 = findViewById(R.id.button1);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(MainActivity.this,HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
