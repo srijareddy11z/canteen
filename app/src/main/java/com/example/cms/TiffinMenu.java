2c193699ca89c3c055ec46fb06424ad217eb724a
srijareddy11z <srijareddysr11.com>
2023-02-15 14:54:37 +0530

    next

--- app/src/main/java/com/example/cms/TiffinMenu.java
+++ app/src/main/java/com/example/cms/TiffinMenu.java
@@ -1,14 +1,13 @@
 package com.example.cms;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class Tiffin extends AppCompatActivity {
+public class TiffinMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tiffin_menu);
     }
 }
